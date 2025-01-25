cac3d7e8ed9b55ac59d038bfceabd83b94d9de3f
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-01-25 20:19:03 +0000

    DRY e finalização do Front-End

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,9 @@
 class ContactsController < ApplicationController
-  def index;  end
+  def index
+    @socialsnet = [
+      { sociallink: "https://www.instagram.com/carlim_11/", socialclass: "ri-instagram-line", title: "Instagram" },
+      { sociallink: "https://github.com/carlosnotlemos", socialclass: "ri-github-line", title: "GitHub" },
+      { sociallink: "https://www.linkedin.com/in/carlos-henrique-b33b78191/", socialclass: "ri-linkedin-box-line", title: "Linkedin" }
+    ]
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
-  def index;  end
+  def index
+    @cards = [
+      { cardicon: "devicon-ruby-plain", cardtitle: "Ruby", carddesc: "Utilizado para front e back end. 41%" },
+      { cardicon: "devicon-html5-plain", cardtitle: "Html", carddesc: "Criação das páginas de forma dinámica. 45%" },
+      { cardicon: "devicon-css3-plain", cardtitle: "CSS", carddesc: "Estilização das páginas junto com Hotwire. 12%" },
+      { cardicon: "devicon-docker-plain", cardtitle: "Docker", carddesc: "Virtualização do sistema para minimizar erros. 2%" }
+    ]
+  end
 end
