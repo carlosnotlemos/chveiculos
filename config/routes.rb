bb9edefe4459cdf5e8100d37c10fbb3c1420c52a
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-02-03 18:30:17 -0300

    Banco de dados e Login feitos...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
 
   # Se for utilizar outras rotas, retirar scope.
@@ -6,5 +7,9 @@ Rails.application.routes.draw do
     resources :academics, path: 'formacoes'
     resources :projects, path: 'projetos'
     resources :contacts, path: 'contatos'
+  end
+
+  namespace :admin do
+    resources :dashboard, only: %i[ index ]
   end
 end
