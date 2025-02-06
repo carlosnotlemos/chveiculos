2e4ea719362d58398205cc95ea872ea15e6ac1f3
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-02-06 19:59:09 -0300

    Verificação de campos nulos e Tradução dos erros

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+  #Validação feita para não passar nenhum valor nulo antes de salvar!
+  validates :language, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :date_p, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Chveiculos
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    #Utilizando o sistema de internacionalização para Traduzir os erros
+    config.i18n.default_locale = :'pt-BR'
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
