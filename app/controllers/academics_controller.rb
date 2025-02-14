425865b3aaabd7c2649a77c4717dc4c5f530cbea
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-02-13 23:14:33 -0300

    add: Adicionando estilização do devise

--- app/controllers/academics_controller.rb
+++ app/controllers/academics_controller.rb
@@ -1,12 +1,6 @@
 class AcademicsController < ApplicationController
   def index
     # Código importante para get no banco de dados!
-    @courses = [
-      { title: "Tecnólogo Superior em Ciência de Dados", platform: "Gran Faculdade", date: "Previsão: 12/2025" },
-      { title: "Análise e Ciência de Dados Pleno", platform: "Plataforma Udemy", date: "Previsão: 02/2025" },
-      { title: "Python 3 Básico", platform: "Plataforma Udemy", date: "Concluído: 2025" },
-      { title: "Ruby on Rails 5.x do início ao fim!", platform: "Plataforma Udemy", date: "Previsão: 03/2025" },
-      { title: "Programador Jr - C#", platform: "Profissionalizante SENAP", date: "Concluído: 03/2015", extra: "Bonus: Banco de dados SQL - Básico / Lógica de Programação" }
-    ]
+    @formacoes = Academic.order(active: :desc, due_date: :desc)
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,5 @@
 class ProjectsController  < ApplicationController
-  def index;  end
+  def index
+    @projetos = Project.order(date_p: :asc).limit(3)
+  end
 end
