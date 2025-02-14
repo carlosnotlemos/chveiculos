7c59b424b32a2f0ef8da17f241a78e66f7bc6f55
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-02-14 15:36:29 -0300

    style: Estilização Final e Finalização do Portifólio

--- app/controllers/academics_controller.rb
+++ app/controllers/academics_controller.rb
@@ -1,6 +1,13 @@
 class AcademicsController < ApplicationController
   def index
-    # Código importante para get no banco de dados!
+    if Academic.all.blank?
+      Academic.create([{title: "Tecnólogo Superior em Ciência de Dados", description: "Gran Faculdade", due_date: "2025-12-24 00:00:00.000000000 +0000", active: false},
+                      {title: "Análise e Ciência de Dados Pleno", description: "Plataforma Udemy", due_date: "2025-03-31 23:00:00.000000000 +0000", active: false},
+                      {title: "Python 3 Básico", description: "Plataforma Udemy", due_date: "2025-01-15 00:10:00.000000000 +0000", active: true},
+                      {title: "Ruby on Rails 5.x do início ao fim!", description: "Plataforma Udemy", due_date: "2025-04-16 00:00:00.000000000 +0000", active: false},
+                      {title: "Programador Jr - C#", description: "Profissionalizante SENAP", due_date: "2015-02-26 00:00:00.000000000 +0000", active: true}])
+    end
+
     @formacoes = Academic.order(active: :desc, due_date: :desc)
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController  < ApplicationController
   def index
+    if Project.all.blank?
+      Project.create({language: "Ruby | Rails",
+                         description: "Primeiro projeto desenvolvido em ruby com framework em Rails.",
+                         image_url: "https://i.postimg.cc/wBkn5RpL/testecard.png" })
+    end
     @projetos = Project.order(date_p: :asc).limit(3)
   end
 end
